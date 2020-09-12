e8a0155a7a23b2690ae26f111eb28cb766ef83e3
Sanyam Sareen <sanyamsareen@Sanyams-MacBook-Air.local>
2020-09-12 18:18:05 -0400

    Add the routes using React Router DOM

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,36 @@
 import React from 'react';
-import './App.css';
+import Navbar from './Navbar';
+import Footer from './Footer'
+
+import Home from '../Pages/Home';
+import Register from '../Pages/Register';
+import About from '../Pages/About';
+import Error from '../Pages/Error';
+import Contact from '../Pages/Contact';
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import OrderNow from '../Pages/OrderNow';
 
 function App() {
   return (
-    <div>Hello World!</div>
+    <Router>
+      <Navbar />
+        <Switch>
+          <Route exact path="/" component = {Home}>
+          </Route>
+          <Route exact path="/about" component = {About}>
+          </Route>
+          <Route exact path="/register" component = {Register}>
+          </Route>
+          <Route exact path="/contact" component = {Contact}>
+          </Route>
+          <Route exact path="/ordernow" component = {OrderNow}>
+          </Route>
+          <Route path="*" component = {Error}>
+          </Route>
+        </Switch>
+      <Footer />
+    </Router>
   );
 }
 
