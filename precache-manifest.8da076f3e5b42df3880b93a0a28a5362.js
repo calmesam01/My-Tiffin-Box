b1e37ee766c65477d1026ef5c69c92ae651c4fe8
Sanyam Sareen <sanyamsareen@Sanyams-MacBook-Air.local>
2020-12-02 15:19:18 -0500

    Updates

---
+++ precache-manifest.8da076f3e5b42df3880b93a0a28a5362.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0ee7ebe928e13dd884da1027756c6c7",
+    "url": "/getmytiffin-ca/index.html"
+  },
+  {
+    "revision": "eb10412b099c3215985f",
+    "url": "/getmytiffin-ca/static/css/main.469d84ce.chunk.css"
+  },
+  {
+    "revision": "9abe9b430b354562f0c7",
+    "url": "/getmytiffin-ca/static/js/2.883d0a93.chunk.js"
+  },
+  {
+    "revision": "7b1affef45e2b57a268c1974299a48a9",
+    "url": "/getmytiffin-ca/static/js/2.883d0a93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb10412b099c3215985f",
+    "url": "/getmytiffin-ca/static/js/main.d52fd646.chunk.js"
+  },
+  {
+    "revision": "6f7aaa083a0f8af4a2f1",
+    "url": "/getmytiffin-ca/static/js/runtime-main.b81061ff.js"
+  },
+  {
+    "revision": "739624b54626f34f71450f17445c5df6",
+    "url": "/getmytiffin-ca/static/media/3_by_4.739624b5.jpg"
+  },
+  {
+    "revision": "a17a00ebc295e25e0aba53c1535d98d8",
+    "url": "/getmytiffin-ca/static/media/bhindi.a17a00eb.svg"
+  },
+  {
+    "revision": "798039579d0333f42bb3eeb740fb3c96",
+    "url": "/getmytiffin-ca/static/media/chilli.79803957.jpg"
+  },
+  {
+    "revision": "48a31508104e871bf69e20192da1fd49",
+    "url": "/getmytiffin-ca/static/media/daal.48a31508.svg"
+  },
+  {
+    "revision": "65c2f2e23fc4929ec437a988932d022b",
+    "url": "/getmytiffin-ca/static/media/dahi.65c2f2e2.svg"
+  },
+  {
+    "revision": "e0db9b8d74be2795adbf89dc44bd8602",
+    "url": "/getmytiffin-ca/static/media/gulab_jamun.e0db9b8d.svg"
+  },
+  {
+    "revision": "7716703ca2e5d3f4393e86474c4c225e",
+    "url": "/getmytiffin-ca/static/media/icon1.7716703c.png"
+  },
+  {
+    "revision": "4b406c3a14ec5742eef9afdaadf4ea58",
+    "url": "/getmytiffin-ca/static/media/icon2.4b406c3a.png"
+  },
+  {
+    "revision": "938698b72f5e8b4a576d27c8a9ace3cf",
+    "url": "/getmytiffin-ca/static/media/icon3.938698b7.png"
+  },
+  {
+    "revision": "79073471471255d63cd11a22a79f9b35",
+    "url": "/getmytiffin-ca/static/media/icon4.79073471.png"
+  },
+  {
+    "revision": "6703e99ae87a5075f1fc31471da43236",
+    "url": "/getmytiffin-ca/static/media/icon5.6703e99a.png"
+  },
+  {
+    "revision": "e19b93311bd5e29b19aa8d16ed4f3599",
+    "url": "/getmytiffin-ca/static/media/icon6.e19b9331.svg"
+  },
+  {
+    "revision": "e8dba2b8590d751e6a20b672e405a1fd",
+    "url": "/getmytiffin-ca/static/media/image.e8dba2b8.png"
+  },
+  {
+    "revision": "a46f157515478a8aa661bc4d69ffc668",
+    "url": "/getmytiffin-ca/static/media/kadi.a46f1575.svg"
+  },
+  {
+    "revision": "9c568d7917ac44c2b7b5f30d6897955b",
+    "url": "/getmytiffin-ca/static/media/matar_paneer.9c568d79.svg"
+  },
+  {
+    "revision": "89bab8fb961d139709bc4cc20ca47c45",
+    "url": "/getmytiffin-ca/static/media/open_tiffin.89bab8fb.png"
+  },
+  {
+    "revision": "ce11d2d2033440693e96da9da6b0cf7e",
+    "url": "/getmytiffin-ca/static/media/rajma.ce11d2d2.svg"
+  },
+  {
+    "revision": "f165c2f9f9d67e8c25ce9038da72f0c3",
+    "url": "/getmytiffin-ca/static/media/rice.f165c2f9.svg"
+  },
+  {
+    "revision": "90a6234938f27399254e49f281a563f8",
+    "url": "/getmytiffin-ca/static/media/roti.90a62349.svg"
+  },
+  {
+    "revision": "fa977c191f163c1da66f659e91bd035d",
+    "url": "/getmytiffin-ca/static/media/salad.fa977c19.svg"
+  },
+  {
+    "revision": "5c95dab4a5fc18635ad85d2545a93179",
+    "url": "/getmytiffin-ca/static/media/shahi_paneer.5c95dab4.svg"
+  },
+  {
+    "revision": "4d197122b814903cc84b4a9da672582e",
+    "url": "/getmytiffin-ca/static/media/shape2.4d197122.png"
+  },
+  {
+    "revision": "0b950049270483f07bedd6c2a58d759e",
+    "url": "/getmytiffin-ca/static/media/spices.0b950049.png"
+  },
+  {
+    "revision": "aad64f0a2e0d6010f2e17ddd8e008234",
+    "url": "/getmytiffin-ca/static/media/tiffin5.aad64f0a.svg"
+  },
+  {
+    "revision": "85ad8ffe3fb9b440b9cf98d1e0b4d8e2",
+    "url": "/getmytiffin-ca/static/media/tiffin_full.85ad8ffe.svg"
+  },
+  {
+    "revision": "0d5b06f402f8f5fd6789e58ef6475000",
+    "url": "/getmytiffin-ca/static/media/todays_special.0d5b06f4.svg"
+  }
+]);
